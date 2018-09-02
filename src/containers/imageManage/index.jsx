e5da1749994340bf16cb2affdb3f4552ad712c2f
zenghuilong <1102645001@qq.com>
2018-09-02 15:22:01 +0800

    添加上传按钮

--- src/containers/imageManage/index.jsx
+++ src/containers/imageManage/index.jsx
@@ -1,17 +1,32 @@
 import React ,{Component} from 'react';
 import LevelBcrumb from '../../component/bcrumb/level1Bcrumb';
-import {Button,Upload,}  from 'antd';
+import {Button,Upload,message}  from 'antd';
  class ImageManage extends Component{
     constructor(props) {
         super(props)
         this.state = {
         }
     }
+    beforeUpload=(file)=>{
+        const isJPG = file.type === 'image/*';
+        if (!isJPG) {
+          message.error('点位示意图只能是图片，请上传图片');
+        }
+        return isJPG;
+      }
     render() {
         return (
              <view>
                  <LevelBcrumb title="视图模版"/>
                 <div>
+                    <Upload 
+                        accept='image/*'
+                        name='file'
+                        action='http://coldcofe.cn:7000/api/15160922dddd52.png?type=UPLOAD_FILE'
+                        // beforeUpload={this.beforeUpload}
+                    >
+                        <Button type="upload">上传点位示意图</Button>
+                    </Upload>
                     <text>上传按钮</text>
                 </div>
                 <div>
