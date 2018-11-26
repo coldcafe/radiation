684585177d9e8d3bbafd64b2ddd8efbdce3775ed
zenghuilong <1102645001@qq.com>
2018-11-26 14:26:17 +0800

    xxx

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,6 +1,6 @@
 const Main = {
 	//http://193.112.148.188:5000/login
-	target: process.env.NODE_ENV !== 'production' ? 'http://120.78.156.32:5000' : 'http://120.78.156.32:5000', //目标网站
+	target: process.env.NODE_ENV !== 'production' ? 'http://120.78.156.32:3000' : 'http://120.78.156.32:3000', //目标网站
 	name: 'Ant Design Admin',
 	downimgUrl: 'http://120.78.156.32:3000/file/files/',
 	uploadimgUrl:'http://120.78.156.32:3000/file/upload',
